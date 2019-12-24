0234d3eba1b7a9a12eb46574d9a94e8281b0a7cd
qianchen <1066476363@qq.com>
2019-12-24 09:43:16 +0800

    add selection

--- Sort/Selection.py
+++ Sort/Selection.py
@@ -4,9 +4,18 @@
 ### first version
 def Selection1(arr):
     length = len(arr)
+    while (length > 0):
+        index = 0
+        for i in range(length):
+            if arr[i] > arr[index]:
+                index = i
+            temp = arr[length-1]
+            arr[length-1] = arr[index]
+            arr[index] = temp
+        length = length - 1
+    print(arr)
     
 
-
 if __name__ == "__main__":
-    arr = [1,2,3,4,5,6,7,8,9,10,13,12,11]
+    arr = [4,2,6,4,5,1,7,3,9,10,13,12,11]
     Selection1(arr)
