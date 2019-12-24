7a0bfd8a34dc9c4f08fc50455e5412b464d4db29
qianchen <1066476363@qq.com>
2019-12-24 11:19:04 +0800

    add Insertion sort

---
+++ Sort/Insertion.py
@@ -0,0 +1,26 @@
+### chan 2019/12/24
+### time o(n^2), space o(1)
+
+### version one 
+def Insertion1(arr):
+
+    length = len(arr)
+    index = 1
+    while length > index :
+        for i in range(index+1):
+            if arr[i]>arr[index] :
+                break
+        arr.insert(i, arr[index])
+        arr.pop(index+1)
+        index = index + 1
+
+    print(arr)
+
+
+
+if __name__ == "__main__":
+    arr = [4,2,6,4,5,1,7,3,9,10,13,12,11]
+    #arr.insert(1,1)
+    #print(arr)
+    Insertion1(arr)
+
