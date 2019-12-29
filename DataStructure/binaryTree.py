107bd39f967d63a447821463acd679691e8c3b69
qianchen <1066476363@qq.com>
2019-12-29 21:38:11 +0800

    添加三种遍历方式

--- DataStructure/binaryTree.py
+++ DataStructure/binaryTree.py
@@ -34,21 +34,21 @@ class BinaryTree():
     def getKeyVal(self):
         return self.key
 
-## 前序遍历
+## 前序遍历：先访问根节点，再访问左子树，最后访问右子树。
 def preOrder(tree):
     if tree :
         print(tree.getKeyVal())
         preOrder(tree.getLeftChild())
         preOrder(tree.getRightChild())
 
-## 后序遍历
+## 后序遍历：先左子树，再右子树，最后根节点。
 def postOrder(tree):
     if tree :
         postOrder(tree.getLeftChild())
         postOrder(tree.getRightChild())
         print(tree.getKeyVal())
 
-## 中序遍历
+## 中序遍历：先左子树，再根节点，最后右子树。
 def midOrder(tree):
     if tree :
         midOrder(tree.getLeftChild())
