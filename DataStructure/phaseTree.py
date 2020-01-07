ef5dbd0859dc8332548d89da64fd94ea9f73c705
qianchen <1066476363@qq.com>
2020-01-07 21:11:04 +0800

    modified phaseTree.py

--- DataStructure/phaseTree.py
+++ DataStructure/phaseTree.py
@@ -6,10 +6,14 @@ import stack
 
 def buildPhaseTree(equation):
     pStack = stack.stack()
+
+    ## etree 保存根节点
     eTree = binaryTree.BinaryTree('')
     currentTree = eTree
 
     for i in equation :
+
+        ##  如果是（，用堆栈保存父节点，currentTree移动到左节点
         if i == '(' :
             #print(i)
             pStack.push(currentTree)
@@ -17,13 +21,15 @@ def buildPhaseTree(equation):
             currentTree = currentTree.getLeftChild()
             #print(pStack.size())     
 
+        ##  如果是数字，对当前节点赋值。一定要记住，赋完值后，要出堆栈，回到父节点。到这里就完成了一个小算式。
         elif i not in ['+', '-', '*', '/', ')']:
             #print(i)
             currentTree.setRootVal(int(i))
             currentTree = pStack.peek()
             pStack.pop()
             #print(pStack.size())     
-            
+
+        ## 如果是运算符，对父节点赋值。用堆栈保存父节点，currentTree移动到右节点。    
         elif i in ['+', '-', '*', '/']:
             #print(i)
             currentTree.setRootVal(i)
@@ -31,7 +37,7 @@ def buildPhaseTree(equation):
             currentTree.insertRight('')
             currentTree = currentTree.getRightChild()
             #print(pStack.size())     
-            
+        ## 如果是），出堆栈，回到父节点。    
         elif i == ')' :
             #print(i)
             currentTree = pStack.peek()
