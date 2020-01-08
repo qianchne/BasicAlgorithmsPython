3ac49aedd2fbf8dd594cdee2c043d53542102c8b
qianchen <1066476363@qq.com>
2020-01-08 18:40:47 +0800

    Q5:longest-palindromic-substring

---
+++ LeetCode/num5.py
@@ -0,0 +1,33 @@
+### chan 2020/1/8
+### problen：https://leetcode-cn.com/problems/longest-palindromic-substring/
+### reference:https://github.com/MisterBooo/LeetCodeAnimation
+
+### version one : 中心扩展法。关键在于解决奇偶个数的回文序列
+def palindromicSubstring(myString):
+
+    ## 记录回文个数
+    maxNum = 1 
+    for i in range(len(myString)*2-1) : ## 中心点有2*n-1个
+        ## 如果i为2倍数，中心点为string中的数字，index设为1，否则设为0.5.
+        if i%2 == 0 :
+            index = 0
+        else:
+            index = 0.5
+        
+        ## 中心扩展并比较
+        while myString[int(i/2-index)] == myString[int(i/2+index)]:
+            if maxNum < index*2 + 1 :
+                maxNum = index*2 + 1
+            index += 1
+
+            ## 控制向两边扩展的范围不超出索引范围
+            if i/2-index < 0 or i/2+index > len(myString)-1 :
+                break
+    return int(maxNum)
+
+if __name__ == "__main__":
+    print(palindromicSubstring('12332611')) 
+
+
+
+        
